3ed3cbcdd5ca0cb4ead73b95645d147784590f71
Saydeezr <saydeezr@gmail.com>
2024-06-03 21:01:59 -0400

    allowed access to public files

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
 sequelize.sync()
  .then (() => {app.listen(PORT, () => {
