eec90b0dbd8c2bf3b2f3cd90a3d5a05324854481
Saydeezr <saydeezr@gmail.com>
2024-06-04 15:25:22 -0400

    fixed pathing mistake

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,5 @@ const apiRoutes = require ('./api');
 
 router.use('/api', apiRoutes);
 
-//I am not sure if this goes here or not?
-router.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-});
-
 
 module.exports = router;
