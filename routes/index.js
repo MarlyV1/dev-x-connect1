9a0f2863198fe374dfbbe00ac36bb50baa237ab2
Saydeezr <saydeezr@gmail.com>
2024-06-04 14:47:55 -0400

    added router and api routes folder

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
+const apiRoutes = require ('./api');
 
+router.use('/api', apiRoutes);
+
+//I am not sure if this goes here or not?
 router.get('/', (req,res) => {
-    res.render('../homepage.html')
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
 });
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'public')));
+
 
 sequelize.sync()
  .then (() => {app.listen(PORT, () => {
