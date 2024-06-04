344eb7a0e04eee3d4528d3bb69b3e1bfec89010f
Saydeezr <saydeezr@gmail.com>
2024-06-03 21:33:27 -0400

    fixed connection error

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,8 @@ if (process.env.DB_URL) {
   } else {
     sequelize = new Sequelize(
       process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PW,
+      process.env.DB_USERNAME,
+      process.env.DB_PASSWORD,
       {
         host: 'localhost',
         dialect: 'postgres',
