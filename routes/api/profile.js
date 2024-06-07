934a9fa71edf781097331a485039ab8b7503f379
Roger_Aponte <rogeraponte10@gmail.com>
2024-06-07 19:42:11 -0400

    added session auth and login sign up profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,22 +1,39 @@
 const router = require('express').Router();
-const Profile = require("../../models")
+const {Profile} = require("../../models")
 //route to create new user
 router.post('/signup', async (req,res) => {
+  try{
   const profileData = await Profile.create(req.body)
+  req.session.save(() => {
+    req.session.user_id = profileData.id;
+    req.session.logged_in = true;
+
+    res.status(200).json(profileData);
+  });
+} catch (err) {
+  res.status(400).json(err);
+}
 })
 
 //route to login existing user
 router.post('/login', async (req,res) => {
   const profileData = await Profile.findOne({where: {username:req.body.username}})
   if(!profileData){
-    res.status(400).json({message:"incorrect username"})
+    res.status(400).json({message:"incorrect credentials"})
     return
+
   }
   const checkpassword = await profileData.validatePassword(req.body.password)
   if(!checkpassword){
-    res.status(400).json({message:"incorrect password"})
+    res.status(400).json({message:"incorrect credentials"})
     return
   }
+  req.session.save(() => {
+    req.session.user_id = profileData.id;
+    req.session.logged_in = true;
+    
+    res.json({ user: profileData, message: 'You are now logged in!' });
+  });
 })
 
 
--- server.js
+++ server.js
@@ -1,16 +1,39 @@
-require("dotenv").config();
+const path = require('path');
 const express = require('express');
+const session = require('express-session');
+const routes = require('./routes');
+
 const sequelize = require('./config/connection');
-const routes = require ('./routes');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
 
-sequelize.sync()
- .then (() => {app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-})});
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
