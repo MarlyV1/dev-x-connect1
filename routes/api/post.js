22aa04bd725cd7fd10fd7be5389891e8c7b01c52
Marly Valcin <marlyvalcin@gmail.com>
2024-06-09 18:46:04 -0400

    updated the post request method

--- routes/api/post.js
+++ routes/api/post.js
@@ -1,11 +1,18 @@
 const router = require('express').Router();
+const { Post, Profile } = require('../../models');
 
 //create new post
 router.post('/', async (req,res) => {
-    const newPost = await Post.create({
+
+    try {
+        const newPost = await Post.create(req.body);
+        res.status(200).json(newPost);
+        console.log('Successfully added to the database');
         
-    })
-})
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
 
 //delet an existing post 
 router.delete('/:id', async (req,res) => {
