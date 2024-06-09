f25f990c817ef35b0360dab31a083e133cf9e9df
Marly Valcin <marlyvalcin@gmail.com>
2024-06-09 18:50:23 -0400

    updated the delete request method

--- routes/api/post.js
+++ routes/api/post.js
@@ -3,12 +3,11 @@ const { Post, Profile } = require('../../models');
 
 //create new post
 router.post('/', async (req,res) => {
-
     try {
         const newPost = await Post.create(req.body);
         res.status(200).json(newPost);
         console.log('Successfully added to the database');
-        
+
     } catch (error) {
         res.status(400).json(error);
     }
@@ -16,11 +15,21 @@ router.post('/', async (req,res) => {
 
 //delet an existing post 
 router.delete('/:id', async (req,res) => {
-    const postData = await Post.destroy({
-        where: {
+    try {
+        const postData = await Post.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
 
-        }
-    })
-})
+        if(!postData) {
+            res.status(404).json({ message: 'No post found with that id'});
+            return;
+        };
+
+    } catch (error) {
+        res.status(500).json(error);
+    } 
+});
 
 module.exports = router;
