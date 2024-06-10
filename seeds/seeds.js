d97e0efe119e1542f4f17592d93beac769d5fed7
Saydeezr <saydeezr@gmail.com>
2024-06-10 17:10:36 -0400

    continued working on seeds

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,4 +1,19 @@
 const sequelize = require('../config/connection');
 const { Profile, Post } = require('../models');
 
-const profileData = require("./profileData.json")
+const profileData = require("./profileData.json");
+const postData = require('./postData.json');
+
+//syncs with database and drops if exists
+const database = async () => {
+    await sequelize.sync({ force: true})
+
+    //creates multiple instances of a Profile and saves in db
+    const profile = await Profile.bulkCreate(profileData, {
+        individualHooks: true,
+        returning: true,
+    });
+    
+}
+
+database();
