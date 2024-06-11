aee74a7e052607a3ccb480ccd6303f8bbf205b4e
Saydeezr <saydeezr@gmail.com>
2024-06-10 20:27:46 -0400

    fixed database connection

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,4 @@
 const Sequelize = require ('sequelize');
 let sequelize;
-if (process.env.DB_URL) {
-    sequelize = new Sequelize(process.env.DB_URL);
-  } else {
-    sequelize = new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USERNAME,
-      process.env.DB_PASSWORD,
-      {
-        host: 'localhost',
-        dialect: 'postgres',
-      },
-    );
-  }
-
+sequelize = new Sequelize(process.env.POSTGRESSURI);
   module.exports= sequelize;
