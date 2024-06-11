365f53e77354f723070cd93180f65b2bbd1cec09
Marly Valcin <marlyvalcin@gmail.com>
2024-06-10 21:24:54 -0400

    updated connections file

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,8 @@
 const Sequelize = require ('sequelize');
 let sequelize;
 
-sequelize = new Sequelize(process.env.POSTGRESSURI);
+sequelize = new Sequelize(process.env.POSTGRESSURI, {
+  dialect: "postgres"
+})
 
   module.exports= sequelize;
