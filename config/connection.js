2450ab33c2dc9ed965834c1f0505c24467688bb4
Marly Valcin <marlyvalcin@gmail.com>
2024-06-10 21:37:25 -0400

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,14 @@
+require('dotenv').config();
 const Sequelize = require ('sequelize');
 let sequelize;
-
-sequelize = new Sequelize(process.env.POSTGRESSURI, {
-  dialect: "postgres"
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL)
+}else {
+  sequelize = new Sequelize(process.env.POSTGRESSURI, {
+  dialect: "postgres",
+  port: 5432
 })
+}
+
 
   module.exports= sequelize;
