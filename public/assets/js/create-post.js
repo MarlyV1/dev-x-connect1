def8b705f35a8cbe5b6f99f6e9b109435423ce06
Marly Valcin <marlyvalcin@gmail.com>
2024-06-14 11:35:16 -0400

    added the poll dropdown options

--- public/assets/js/create-post.js
+++ public/assets/js/create-post.js
@@ -2,6 +2,12 @@ let title = document.querySelector('.title');
 let textarea = document.querySelector('.textarea');
 const submitBtn = document.querySelector('.submit-post');
 
+const dropdown = document.querySelector('.dropdown');
+const twoPollChoices = document.querySelector('.two-poll-choices');
+const pollChoiceThree = document.querySelector('.poll-choice-three');
+const pollChoiceFour = document.querySelector('.poll-choice-four');
+
+
 //Formatting for the Quill text editor
 const toolbarOptions = [
   [{ 'header': [1, 2, false] }],
@@ -46,3 +52,29 @@ const handleNewPost = async () => {
   const data = await makePost(newPost);
   return data;
 }
+
+//Dropdown that will display the amount of poll options
+const dropdownOption = (value) => {
+  if(value === 2) {
+    twoPollChoices.style.display ='block';
+    pollChoiceThree.style.display ='none';
+    pollChoiceFour.style.display ='none';
+  } else if(value === 3) {
+    twoPollChoices.style.display ='block';
+    pollChoiceThree.style.display ='block';
+    pollChoiceFour.style.display ='none';
+  } else if(value === 4) {
+    twoPollChoices.style.display ='block';
+    pollChoiceThree.style.display ='block';
+    pollChoiceFour.style.display ='block';
+  } else {
+    twoPollChoices.style.display ='none';
+    pollChoiceThree.style.display ='none';
+    pollChoiceFour.style.display ='none';
+  }
+}
+
+dropdown.addEventListener('change', (e) => {
+  e.preventDefault;
+  dropdownOption(parseInt(dropdown.value));
+})
